51cebc54f672212e4db4125e70baaf87eda9330a
Daniel Gilmour <daniel.gilmour@canterbury.ac.nz>
2023-11-10 15:22:48 +1300

    added navbar

--- src/components/home/header/header.js
+++ src/components/home/header/header.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const stylez = {
+    backgroundColor: '#20a608'
+}
 
 export default function header() {
   return (
-    <div>
-      <p>Something at the Top</p>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar style={stylez}>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Header
+        </Typography>
+        <Button color="inherit">Something</Button>
+      </Toolbar>
+    </AppBar>
+  </Box>
   )
 }
